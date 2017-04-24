93ee0fb1c2f11b84ee26adbf0f011b5b8912f8b1
Carl <carlmorner@hotmail.com>
2017-04-24 02:52:28 +0200

    myCar

--- client/scripts/bumperCars.js
+++ client/scripts/bumperCars.js
@@ -2,13 +2,21 @@ var bc = bc || {};
 
 bc.main = (function () {
 	var obj = {};
+	var framesPerSecond = 30;
+	// Calculate intervalSpace to achieve desired fps
+	var interValSpace = 1000/framesPerSecond;
+
+	var currentInterval;
+
 
 	obj.init = function () {
 		// Attach handlers for key events
 		bc.controls.bindKeyEvents();
+		currentInterval = setInterval(this.gameLoop, interValSpace);
 	}
 
 	obj.gameLoop = function () {
+	
 		bc.canvas.drawGame();
 	}
 
--- client/scripts/myCar.module.js
+++ client/scripts/myCar.module.js
@@ -33,9 +33,61 @@ bc.myCar = (function () {
 
 	// Handles user input relating to the direction of the car
 	obj.directionInputHandler = function (direction) {
+		// How much the carVectors are changed
+		var accelerationConstant = 0.6;
+		var maxSpeed = 4; // Max speed change of pixels per frame
+		switch (direction) {
+			case 'left':
+				if (Math.abs(carVectors.x) < maxSpeed){
+					carVectors.x = carVectors.x - accelerationConstant;
+				}
+				break;
+			case 'up':
+				if (Math.abs(carVectors.y) < maxSpeed){
+					carVectors.y = carVectors.y - accelerationConstant;
+				}
+				break;
+			case 'right':
+				if (Math.abs(carVectors.x) < maxSpeed){
+					carVectors.x = carVectors.x + accelerationConstant;
+				}
+				break;
+			case 'down':
+				if (Math.abs(carVectors.y) < maxSpeed){
+					carVectors.y = carVectors.y + accelerationConstant;
+				}
+				break;
+		}
+
 		console.log(direction);
 		// Changes carVectors based upon which direction is received
 		// Make sure the car wont override its speedLimit or maybe there shouldn't be one :)
+	}
+
+	// Affect vectors with friciton
+	obj.applyFriction = function () {
+		// Decides how much the car will break when no force is applied
+		var frictionConstant = 0.2;
+		// Apply friction in the opposite way of traveling distance
+		// only apply if car is standing still (vector is 0)
+		if (carVectors.x < 0){
+			carVectors.x += frictionConstant;
+		} else if (carVectors.x > 0) {
+			carVectors.x -= frictionConstant;
+		}
+
+		if (carVectors.y < 0){
+			carVectors.y += frictionConstant;
+		} else if (carVectors.y > 0) {
+			carVectors.y -= frictionConstant;
+		}
+
+	}
+
+	// Calculate new position of car based of current carVectors
+	obj.moveCar = function () {
+		currentCenterPosition.x += carVectors.x;
+		currentCenterPosition.y += carVectors.y;
 	}
 
 	// Returns an position object
